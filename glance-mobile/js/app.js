608f7ea4118d70c82d0a1a3748903af8e8cafd2c
unknown <ravi.mamidipalli@gmail.com>
2015-09-27 11:53:46 +0530

    Adding glance code

--- glance-mobile/js/app.js
+++ glance-mobile/js/app.js
@@ -1,67 +1,42 @@
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
+angular.module('glance', ['ionic', 'glance.controllers'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+        controller: 'AppCtrl'
+      })
+      .state('app.map', {
+        url: "/map",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/map.html",
+            controller: 'MapCtrl'
+          }
         }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
+      })
+      .state('app.profile', {
+        url: "/profile",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/profile.html",
+            controller: 'AppCtrl'
+          }
         }
-      }
-    })
+      })
+      .state('splash', {
+        url: '/splash',
+        templateUrl: "templates/splash.html"
+      })
+      .state('signin', {
+        url: "/signin",
+        templateUrl: "templates/signin.html"
+      })
+      .state('register', {
+        url: "/register",
+        templateUrl: "templates/register.html"
+      })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
+    $urlRouterProvider.otherwise('/splash');
   });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
--- glance-mobile/js/controllers.js
+++ glance-mobile/js/controllers.js
@@ -1,82 +1,86 @@
-angular.module('starter.controllers', [])
+angular.module('glance.controllers', ['ngResource'])
+.factory("glancecreateaccount", ["$resource",
+  function($resource) {
+    return $resource("https://lu4sdhzvbi.execute-api.us-west-2.amazonaws.com/dev/account/create");
+  }
+])
+.factory("glancesignin", ["$resource",
+  function($resource) {
+    return $resource("https://lu4sdhzvbi.execute-api.us-west-2.amazonaws.com/dev/account/signin");
+  }
+])
+.factory("glancepub", ["$resource",
+  function($resource) {
+    return $resource("https://lu4sdhzvbi.execute-api.us-west-2.amazonaws.com/dev/location/pub", {}, {
+        'save': {method:'POST', isArray: true}
+    });
+  }
+])
+.factory("glancesms", ["$resource",
+  function($resource) {
+    return $resource("https://lu4sdhzvbi.execute-api.us-west-2.amazonaws.com/dev/sms/send"
+    );
+  }
+])
+.factory("glancemail", ["$resource",
+  function($resource) {
+    return $resource("https://lu4sdhzvbi.execute-api.us-west-2.amazonaws.com/dev/mail/confirm"
+    );
+  }
+])
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout, $state, glancemail, $http, glancecreateaccount, glancesignin) { 
+    $scope.loginData = {};
+    $scope.registerData = {};
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+    $scope.doLogin = function () {
+      console.log('Authenticating '+ $scope.loginData.email);
+      $http.defaults.headers.common['x-api-key'] = 'SNriDHrilP2i8MqyAypVtA8ZsyzVdy39z7USppI7';
+      glancesignin.save({"email":$scope.loginData.email,"password":$scope.loginData.password}, function (authData, error) {
+        if (authData.token) {
+          console.log("Authenticated successfully : "+JSON.stringify(authData));
+          $state.go("app.map");
+        } else {
+          console.log("Login Failed : " + error);
+          $state.go("splash");
+        }
+      });
+    };
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    $scope.doRegister = function () {
+      console.log('Registering...'+JSON.stringify($scope.registerData));
+      $http.defaults.headers.common['x-api-key'] = 'SNriDHrilP2i8MqyAypVtA8ZsyzVdy39z7USppI7';
+      glancecreateaccount.save({"email":$scope.registerData.email,"name":$scope.registerData.name,"phone":$scope.registerData.phone,"password":$scope.registerData.password}, function(res){
+            console.log("Account Created ####:#### "+JSON.stringify(res));
+            if(res != null && res.uid != null){
+              console.log("userData New "+JSON.stringify(res));
+              
+              $http.defaults.headers.common['x-api-key'] = 'SNriDHrilP2i8MqyAypVtA8ZsyzVdy39z7USppI7';
+              var res = glancemail.save({"mailid":$scope.registerData.email,"uid":res.uid});
+              console.log("Sent Email : "+JSON.stringify(res));
+              
+              $state.go("signin");
+            } else {
+              $scope.error = error;
+              console.log("Registration failed - "+(res != null && res.error != null) ? res.error : "");
+            }
+      });
+    };
 
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+    $scope.logout = function () {
+      $state.go("splash");
+    };
 })
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-})
-
-.controller('MapCtrl', function($scope, $ionicLoading) {
-  $scope.mapCreated = function(map) {
-    $scope.map = map;
-  };
-
-  $scope.centerOnMe = function () {
-    console.log("Centering");
-    if (!$scope.map) {
-      return;
+.controller('MapCtrl', function ($scope, $ionicLoading, $compile, $http, glancepub, glancesms, $cordovaGeolocation, $cordovaContacts) {
+    if (navigator.geolocation) {
+      initializeMap();
+      
+      $cordovaGeolocation
+      .getCurrentPosition({enableHighAccuracy: true})
+      .then(function (position) {
+        displayAndWatch(position, $http, glancepub, glancesms, $cordovaGeolocation, $cordovaContacts);
+      }, locError);
+      
+    } else {
+      alert("Your browser does not support the Geolocation API");
     }
-
-    $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
-
-    navigator.geolocation.getCurrentPosition(function (pos) {
-      console.log('Got pos', pos);
-      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      $scope.loading.hide();
-    }, function (error) {
-      alert('Unable to get location: ' + error.message);
-    });
-  };
 });
