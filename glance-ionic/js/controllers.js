df34190c91a402e944cd8708be09ffa14b768259
Vishnu584 <Vishnu584@gmail.com>
2015-10-10 21:02:49 +0530

    Map Share Code

--- glance-ionic/js/controllers.js
+++ glance-ionic/js/controllers.js
@@ -1,17 +1,17 @@
 
-angular.module('starter.controllers', ['ionic','firebase'])
+angular.module('starter.controllers', ['ionic', 'firebase'])
 
-.controller('AppCtrl', function($scope, $state, $firebase, $ionicModal, $timeout, Auth, glancemail, $http, glancecreateaccount, glancesignin, resetpassword) {
-  
-  $scope.loginData = {};
+  .controller('AppCtrl', function ($scope, $state, $firebase, $ionicModal, $timeout, Auth, glancemail, $http, glancecreateaccount, glancesignin, resetpassword) {
+
+    $scope.loginData = {};
     $scope.registerData = {};
-    
+
     $scope.doLoginEmail = function () {
-     console.log('Authenticating '+ $scope.loginData.email);
+      console.log('Authenticating ' + $scope.loginData.email);
       $http.defaults.headers.common['x-api-key'] = 'SNriDHrilP2i8MqyAypVtA8ZsyzVdy39z7USppI7';
-      glancesignin.save({"email":$scope.loginData.email,"password":$scope.loginData.password}, function (authData, error) {
+      glancesignin.save({ "email": $scope.loginData.email, "password": $scope.loginData.password }, function (authData, error) {
         if (authData.token) {
-         console.log("Authenticated successfully : "+JSON.stringify(authData));
+          console.log("Authenticated successfully : " + JSON.stringify(authData));
           $state.go("app.map");
         } else {
           console.log("Login Failed : " + error);
@@ -19,108 +19,135 @@ angular.module('starter.controllers', ['ionic','firebase'])
         }
       });
     };
-  
-  
-  $scope.doSocialLogin = function(authMethod) {
-    console.log("In signin");
-    Auth.$authWithOAuthPopup(authMethod).then(function(authData) {
-      // User successfully logged in
+
+
+    $scope.doSocialLogin = function (authMethod) {
+      console.log("In signin");
+      Auth.$authWithOAuthPopup(authMethod).then(function (authData) {
+        // User successfully logged in
         console.log("$authWithOAuthPopup***");
-      $state.go("app.map");
-    }).catch(function(error) {
-      if (error.code === "TRANSPORT_UNAVAILABLE") {
-        Auth.$authWithOAuthRedirect(authMethod).then(function(authData) {
-          // User successfully logged in. We can log to the console
-          // since we’re using a popup here
-           console.log("$authWithOAuthRedirect***");
-          console.log(authData);
-          $state.go("app.map");
-        });
-      } else {
-        // Another error occurred
-        console.log(error);
-      }
-    });
-  };
-  
-  $scope.doRegister = function () {
-      console.log('Registering...'+JSON.stringify($scope.registerData));
+        $state.go("app.map");
+      }).catch(function (error) {
+        if (error.code === "TRANSPORT_UNAVAILABLE") {
+          Auth.$authWithOAuthRedirect(authMethod).then(function (authData) {
+            // User successfully logged in. We can log to the console
+            // since we’re using a popup here
+            console.log("$authWithOAuthRedirect***");
+            console.log(authData);
+            $state.go("app.map");
+          });
+        } else {
+          // Another error occurred
+          console.log(error);
+        }
+      });
+    };
+
+    $scope.doRegister = function () {
+      console.log('Registering...' + JSON.stringify($scope.registerData));
       $http.defaults.headers.common['x-api-key'] = 'SNriDHrilP2i8MqyAypVtA8ZsyzVdy39z7USppI7';
-      glancecreateaccount.save({"email":$scope.registerData.email,"name":$scope.registerData.name,"phone":$scope.registerData.phone,"password":$scope.registerData.password}, function(res){
-            console.log("Account Created ####:#### "+JSON.stringify(res));
-            if(res != null && res.uid != null){
-              console.log("userData New "+JSON.stringify(res));
-              
-              $http.defaults.headers.common['x-api-key'] = 'SNriDHrilP2i8MqyAypVtA8ZsyzVdy39z7USppI7';
-              var res = glancemail.save({"mailid":$scope.registerData.email,"uid":res.uid});
-              console.log("Sent Email : "+JSON.stringify(res));
-              
-              $state.go("signin");
-            } else {
-              //$scope.error = error;
-              console.log("Registration failed - "+(res != null && res.error != null) ? res.error : "");
-            }
+      glancecreateaccount.save({ "email": $scope.registerData.email, "name": $scope.registerData.name, "phone": $scope.registerData.phone, "password": $scope.registerData.password }, function (res) {
+        console.log("Account Created ####:#### " + JSON.stringify(res));
+        if (res != null && res.uid != null) {
+          console.log("userData New " + JSON.stringify(res));
+
+          $http.defaults.headers.common['x-api-key'] = 'SNriDHrilP2i8MqyAypVtA8ZsyzVdy39z7USppI7';
+          var res = glancemail.save({ "mailid": $scope.registerData.email, "uid": res.uid });
+          console.log("Sent Email : " + JSON.stringify(res));
+
+          $state.go("signin");
+        } else {
+          //$scope.error = error;
+          console.log("Registration failed - " + (res != null && res.error != null) ? res.error : "");
+        }
       });
     };
 
     $scope.logout = function () {
       $state.go("splash");
     };
-    
-     $scope.resetPassword = function () {
-     console.log('Resetting password - '+ $scope.loginData.email);
+
+    $scope.resetPassword = function () {
+      console.log('Resetting password - ' + $scope.loginData.email);
       $http.defaults.headers.common['x-api-key'] = 'SNriDHrilP2i8MqyAypVtA8ZsyzVdy39z7USppI7';
-      resetpassword.save({"email":$scope.loginData.email}, function(error) {
-      if (error === null) {
-        console.log("Password reset email sent successfully");
-        $state.go("signin");
-      } else {
-        console.log("Error sending password reset email:", error);
-      }
+      resetpassword.save({ "email": $scope.loginData.email }, function (error) {
+        if (error === null) {
+          console.log("Password reset email sent successfully");
+          $state.go("signin");
+        } else {
+          console.log("Error sending password reset email:", error);
+        }
       });
     };
 
-})
+  })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+  .controller('PlaylistsCtrl', function ($scope) {
+    $scope.playlists = [
+      { title: 'Reggae', id: 1 },
+      { title: 'Chill', id: 2 },
+      { title: 'Dubstep', id: 3 },
+      { title: 'Indie', id: 4 },
+      { title: 'Rap', id: 5 },
+      { title: 'Cowbell', id: 6 }
+    ];
+  })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-})
+  .controller('PlaylistCtrl', function ($scope, $stateParams) {
+  })
 
-.controller('MapCtrl', function($scope, $ionicLoading) {
-  $scope.mapCreated = function(map) {
-    $scope.map = map;
-  };
+  .controller('MapCtrl', function ($scope, $ionicLoading, $http, glancepub) {
 
-  $scope.centerOnMe = function () {
-    console.log("Centering");
-    if (!$scope.map) {
-      return;
+    $scope.mapCreated = function (map) {
+      console.log("*** In map created ***");
+      $scope.map = map;
+
+
+    };
+
+    $scope.centerOnMe = function () {
+      console.log("Centering");
+      if (!$scope.map) {
+        return;
+      }
+
+      $scope.loading = $ionicLoading.show({
+        content: 'Getting current location...',
+        showBackdrop: false
+      });
+
+      navigator.geolocation.getCurrentPosition(function (pos) {
+        console.log('Got pos', pos);
+        $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        $scope.loading.hide();
+      }, function (error) {
+        alert('Unable to get location: ' + error.message);
+      });
+    };
+
+    $scope.shareMe = function () {
+      console.log("Share Me");
+
+      navigator.geolocation.watchPosition(function (pos) {
+        
+        $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        console.log("Lat:" + pos.coords.latitude + " Long:" + pos.coords.longitude);
+        var myLocation = new google.maps.Marker({
+          position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+          map: $scope.map
+        });
+
+        $http.defaults.headers.common['x-api-key'] = 'SNriDHrilP2i8MqyAypVtA8ZsyzVdy39z7USppI7';
+        console.log("Map Lat-->" + $scope.map.getCenter().lat());
+        console.log("Map Lng-->" + $scope.map.getCenter().lng());
+        var msg = '{"lat":' + $scope.map.getCenter().lat() + ',"lng":' + $scope.map.getCenter().lng() + '}';
+        var message = JSON.parse(msg);
+        glancepub.save({ "channel": "my_channel", "message": message }, function (res) {
+          console.log("Published : " + JSON.stringify(res));
+        })
+      });
     }
-
-    $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
-
-    navigator.geolocation.getCurrentPosition(function (pos) {
-      console.log('Got pos', pos);
-      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      $scope.loading.hide();
-    }, function (error) {
-      alert('Unable to get location: ' + error.message);
-    });
-  };
-});
+  });
 
 
 /*angular.module('starter.controllers', [])
--- glance-ionic/js/directives.js
+++ glance-ionic/js/directives.js
@@ -1,44 +1,47 @@
 angular.module('starter.directives', [])
 
-.directive('map', function() {
-  return {
-    restrict: 'E',
-    scope: {
-      onCreate: '&'
-    },
-    link: function ($scope, $element, $attr) {
-      function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(43.07493, -89.381388),
-          zoom: 16,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map($element[0], mapOptions);
-        
-        navigator.geolocation.getCurrentPosition(function(pos) {
+  .directive('map', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        onCreate: '&'
+      },
+      link: function ($scope, $element, $attr) {
+        function initialize() {
+          var mapOptions = {
+            center: new google.maps.LatLng(43.07493, -89.381388),
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          };
+          var map = new google.maps.Map($element[0], mapOptions);
+
+          navigator.geolocation.getCurrentPosition(function (pos) {
             map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            console.log("Lat:" + pos.coords.latitude + " Long:" + pos.coords.longitude);
             var myLocation = new google.maps.Marker({
-                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-                map: map,
-                title: "My Location"
+              position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+              map: map,
+              title: "My Location"
             });
-        });
+
+            
+          });
         
-  
-        $scope.onCreate({map: map});
+      $scope.onCreate({ map: map });
+      
 
-        // Stop the side bar from dragging when mousedown/tapdown on the map
-        google.maps.event.addDomListener($element[0], 'mousedown', function (e) {
-          e.preventDefault();
-          return false;
-        });
-      }
+          // Stop the side bar from dragging when mousedown/tapdown on the map
+          google.maps.event.addDomListener($element[0], 'mousedown', function (e) {
+            e.preventDefault();
+            return false;
+          });
+        }
 
-      if (document.readyState === "complete") {
-        initialize();
-      } else {
-        google.maps.event.addDomListener(window, 'load', initialize);
+        if (document.readyState === "complete") {
+          initialize();
+        } else {
+          google.maps.event.addDomListener(window, 'load', initialize);
+        }
       }
     }
-  }
-});
+  });
